0e6bfb7edd38ed17df1c559376a6c00aa001a074
AysArzu <aysebattal@gmail.com>
2023-02-04 12:36:12 +0300

    02-03

--- src/main/java/replit/list/EngList01.java
+++ src/main/java/replit/list/EngList01.java
@@ -1,5 +1,7 @@
 package replit.list;
 
+import java.util.Scanner;
+
 public class EngList01 {
     /*
         Write a Java program to get a String from user as input and find
@@ -10,6 +12,22 @@ public class EngList01 {
         maximum occurring character is : a
      */
     public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+       String input= scan.nextLine().toLowerCase();
 
+        for (int i = 0; i <input.length() ; i++) {
+            int count=0;
+           char c= input.charAt(i);
+            for (int j = 0; j <input.length() ; j++) {
+               if( input.charAt(j)==c){
+                  count++ ;
+                }
+
+            } System.out.print(c+" "+count);
+        }
+
+
+
+       // System.out.println(" maximum occurring character is :");
     }
 }
